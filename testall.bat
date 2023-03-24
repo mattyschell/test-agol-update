fc0bda54829146da4e6644d55b798ec5b9dbe338
Matt Schell <mschell@doitt.nyc.gov>
2023-03-24 10:54:55 -0400

    completes first three proxy tests

--- testall.bat
+++ testall.bat
@@ -1,1 +1,7 @@
-CALL python test_arcgisonline.py
+REM review these
+SET NOTIFYFROM=xx@xxx.xxx.xxx
+SET NOTIFY=xxxx@xxx.xxx.xxx
+SET SMTPFROM=xxxxxx.xxxxx
+SET FMEHOME="C:\Program Files\FME\"
+%FMEHOME%fme.exe test\test-proxy.fmw
+REM CALL python test_arcgisonline.py
